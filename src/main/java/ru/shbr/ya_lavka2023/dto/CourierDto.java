ef986a5b6f97ef51926e41ab48698e9950c3ddd8
alexanderbibik <sbibik2000@mail.ru>
2024-06-11 16:27:50 +0300

    added javadoc for main CourierDto

--- src/main/java/ru/shbr/ya_lavka2023/dto/CourierDto.java
+++ src/main/java/ru/shbr/ya_lavka2023/dto/CourierDto.java
@@ -1,4 +1,21 @@
 package ru.shbr.ya_lavka2023.dto;
 
-public class CourierDto {
+import ru.shbr.ya_lavka2023.entity.CourierTypeEnum;
+
+import java.util.Set;
+
+/**
+ * Dto Курьера {@link ru.shbr.ya_lavka2023.entity.Courier} со всеми полями
+ *
+ * @param id              идентификатор курьера
+ * @param courierTypeEnum тип передвижения
+ * @param areas           районы
+ * @param loadCapacity    грузоподъемность
+ * @param workingHours    промежутки работы
+ */
+public record CourierDto(Long id,
+                         CourierTypeEnum courierTypeEnum,
+                         Set<Integer> areas,
+                         int loadCapacity,
+                         Set<String> workingHours) {
 }
